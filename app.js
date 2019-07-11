c4a0dd96febabb8f539d2569314b66b3397329ab
Samuel Peters <peterssam@me.com>
2019-07-10 21:46:15 -0700

    Changing the listening port

--- app.js
+++ app.js
@@ -6,18 +6,20 @@ const passport = require('passport');
 const mongoose = require('mongoose');
 const config = require('./config/database');
 
+
 // Connect To Database
 mongoose.Promise = require('bluebird');
 mongoose.connect(config.database, { useMongoClient: true, promiseLibrary: require('bluebird') })
   .then(() => console.log(`Connected to database ${config.database}`))
   .catch((err) => console.log(`Database error: ${err}`));
 
+
 const app = express();
 
 const users = require('./routes/users');
 
 // Port Number
-const port = 3000;
+const port = 80;
 
 // CORS Middleware
 app.use(cors());
